10e99c15cd9057760d45f622025819a98113f8cc
Maira Molina <mpmolina1320@gmail.com>
2025-10-23 16:06:20 +0200

    Player Cam & Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,40 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private Transform orientation;
+    [SerializeField] private float drag = 2f;
     
-    private horizontalInput;
-    private verticalInput;
+    private float horizontalInput;
+    private float verticalInput;
     private Vector3 moveDirection;
     private Rigidbody rb;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MyInput();
+        rb.linearDamping = drag;
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        //rb.MovePosition(rb.position + moveDirection.normalized * moveSpeed * Time.deltaTime);
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }
